4d65f35ac41abf645c11d68bb914aee364e0b1f4
PRO0FESSOR <siddharth96549@gmail.com>
2024-02-18 16:38:57 +0530

    first component and export of component

--- level_1/random/src/App.js
+++ level_1/random/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import Profileer from './components/Profileer';
 
 function App() {
   return (
     <>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
 
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
+    <Profile/>
+    <Profileer/>
+    
     </>
   );
 }
---
+++ level_1/random/src/components/Profileer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Profileer() {
+  return (
+    <div>Profileer</div>
+  )
+}
